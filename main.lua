75a8c15eefb6e05830c4ae0042a9fa153ff8742f
TechniticalBeeping <business.skysmp@gmail.com>
2026-09-12 23:58:58 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+writefile('landryhaxx/key.txt', lp_key)
+
+queue_on_teleport([=[
+    lp_key = readfile('landryhaxx/key.txt')
+
+    task.spawn(function()
+        loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/59810384473976980152"))()
+    end)()
+
+    repeat task.wait() until getgenv().Bypassed
+
+    loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/99779786104572102737"))()
+]=])
+
+rconsoleprint("connect to a server now")
