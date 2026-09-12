1118b91c1ef8e503559a1627288e2c273920c8af
TechniticalBeeping <business.skysmp@gmail.com>
2026-09-13 02:24:14 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,17 @@
-if not lp_key then return end
+if not script_key then return end
 
-writefile('landryhaxx/key.txt', lp_key)
+writefile('landryhaxx/key.txt', script_key)
 
 queue_on_teleport([[
-    lp_key = readfile('landryhaxx/key.txt')
+    script_key = readfile('landryhaxx/key.txt')
 
     task.spawn(function()
-        loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/59810384473976980152"))()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/259f2737dc5cf5778cb87c834144a903.lua"))()
     end)
 
     repeat task.wait() until getgenv().bypassloaded
 
-    loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/99779786104572102737"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/af3a50145af3c7bd2dda5350aa974db3.lua"))()
 ]])
 
 rconsoleprint('connect to a server now')
