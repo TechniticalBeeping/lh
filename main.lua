8e7fb3fe2f4707af39496818cbfd59644a03452a
TechniticalBeeping <business.skysmp@gmail.com>
2026-09-22 23:34:47 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ queue_on_teleport([[
     script_key = readfile('landryhaxx/key.txt')
 
     task.spawn(function()
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2c0b62621c9ff68acd26c3a220375a81.lua"))()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/4f8fea439b937332e4dc5f226ae9d6f7.lua"))()
     end)
 
     repeat task.wait() until getgenv().bypassloaded
